9096f4fb7d107a324123c27b36b522a0ba4d0180
MonicaIzzo <info@monicaizzo.net>
2023-06-25 20:06:13 +0200

    abbandono i data in file esterno (mi da errore)

--- js/script.js
+++ js/script.js
@@ -30,23 +30,45 @@ console.log('Vue OK, Vue');
 // # inizializzo Vue JS
 
     //Estrapolo il metodo createApp
-/*    const app = Vue.createApp ({
+/*  const app = Vue.createApp({
         name: 'Carousel',
-        images() {
-            return {
-                CurrentIndex: 0,
-                image: images.image
-                }
-            },
-        });
+        data() {
+        return {
+            currentIndex: 0,
+            pictures: images
+            }
+        },
+    });  
 */
 
     const app = Vue.createApp({
         name: 'Carousel',
         data() {
-        return {
-            currentIndex: 0,
-            pictures: ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/05.webp', 'img/05.webp']
+            return {
+                currentIndex: 0,
+                pictures: [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+]
             }
         },
     });    
