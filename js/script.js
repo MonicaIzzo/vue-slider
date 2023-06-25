4346f5dde0f8ae001ee03d072b8e3b706976918a
MonicaIzzo <info@monicaizzo.net>
2023-06-25 09:05:37 +0200

    riprendo l'esercizio da dove l'avevo lasciato

--- js/script.js
+++ js/script.js
@@ -1,50 +1,24 @@
 /*
-# Carosello Vue JS
-
-Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue e un array di oggetti. Le immagini sono le stesse della volta scorsa.
-:avviso: Attenzione: ricordate di copiare il template HTML dall'esercizio precedente aiutandovi con la console.
-
-Bonus:
-1 - al click su una thumb, visualizzare in grande l'immagine corrispondente
-2 - applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
-3 - quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
-Consigli del giorno:
-regola d'oro: riciclare ovunque possibile! Se stiamo ripetendo della logica possiamo aiutarci sicuramente centralizzando il codice!
-il riciclo spesso va a braccetto con le funzioni! Sapendole sfruttare bene, l'esercizio si riduce a poche righe.
-
-<br>
-<br>
-
-- **0**
-  - Ricreo il template statico della pagina
-- **1**
-  - Mi riporto l'arrey di oggetto sotto data
-- **2**
-  - 
-- **3**
-  - 
-  **4**
-  - 
- **5**
-  - 
-**FINE**  .
+Apriamo la repo e facciamo subito un commit con il solito boilerplate.
+Importiamo Vue tramite CDN (possiamo copiarlo dalle slide, da CDNjs oppure dal questa pagina della documentazione ufficiale).
+Inizializziamo e montiamo un'app Vue.
+Mettiamo in pagina un h1 il cui contenuto testuale deve derivare dai data di Vue.
+BONUS: proviamo ad aggiungere un'immagine, derivante anch'essa dai dati di Vue.
 
 */
 
 // # verifiche JS
-console.log('JS OK')
+console.log('JS OK');
 console.log('Vue OK, Vue');
 
 // # inizializzo Vue JS
 
     //Estrapolo il metodo createApp
-    const {createApp} = Vue;
-
-    //inizializzo Vue JS
-    const app = createApp({
+    const app = Vue.createApp ({
         data() {
             return {
-                imgVueJs: "vuejs-development-malaysia.jpeg"
+                message: 'Hello Vue JS',
+                imgVueJs: 'img/vuejs-development-malaysia.jpeg'
             }
         }
     });
